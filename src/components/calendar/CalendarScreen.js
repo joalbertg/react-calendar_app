5f2659aff32999fd79bbc5c65d4a6407d091c775
Joalbert <joalbertgonzalez@gmail.com>
2020-07-09 01:10:26 -0500

    [ADD]🚀 Spanish messages added

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
-import { Navbar } from '~components/ui/Navbar';
-
+import 'moment/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { Navbar } from '~components/ui/Navbar';
+import { messages } from '~helpers/calendar-messages-es';
+
+moment.locale('es');
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
@@ -13,10 +17,26 @@ const events = [{
   title: 'Random Birthday',
   start: moment().toDate(),
   end: moment().add(2, 'hours').toDate(),
-  bgcolor: '#fafafa'
+  bgcolor: '#fafafa',
+  notes: 'buy the cake'
 }];
 
 const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#367cf7',
+      borderRadius: 0,
+      opacity: 0.8,
+      display: 'block',
+      color: 'white'
+    };
+
+    //console.log(event, start, end, isSelected);
+    return {
+      style
+    };
+  };
+
   return(
     <div className='calendar-screen'>
       <Navbar />
@@ -25,6 +45,8 @@ const CalendarScreen = () => {
         events={events}
         startAccessor='start'
         endAccessor='end'
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   );
